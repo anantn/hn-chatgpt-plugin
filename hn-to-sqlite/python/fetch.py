2babe2345c5dcb000d9d223851cde0fbd49c6e1a
Anant Narayanan <anant@kix.in>
2023-04-21 01:37:14 -0700

    This version gets us 3k items/s

--- hn-to-sqlite/python/fetch.py
+++ hn-to-sqlite/python/fetch.py
@@ -4,10 +4,10 @@ import aiosqlite
 from tqdm import tqdm
 
 ITEM_URL = "https://hacker-news.firebaseio.com/v0/item/{id}.json"
-START_ID = 35650333
+START_ID = 30000
 END_ID = 1
-BATCH_SIZE = 1000
-PARALLEL = 100
+BATCH_SIZE = 2048
+NUM_WORKERS = 256
 
 
 async def fetch_item(session, id):
@@ -17,52 +17,66 @@ async def fetch_item(session, id):
         return None
 
 
-async def insert_item(db, item):
-    async with db.execute("""
+async def insert_items_batch(db, items_batch):
+    items_data = []
+    kids_data = []
+
+    for item in items_batch:
+        items_data.append((item['id'], item.get('deleted', None), item['type'], item.get('by', None), item['time'], item.get('text', None),
+                           item.get('dead', None), item.get('parent', None), item.get(
+                               'poll', None), item.get('url', None),
+                           item.get('score', None), item.get('title', None), ','.join(
+                               map(str, item.get('parts', []))) if item.get('parts') else None,
+                           item.get('descendants', None)))
+
+        if 'kids' in item:
+            for order, kid_id in enumerate(item['kids']):
+                kids_data.append((item['id'], kid_id, order))
+
+    async with db.executemany("""
         INSERT OR IGNORE INTO items (id, deleted, type, by, time, text, dead, parent, poll, url, score, title, parts, descendants)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    """, (item['id'], item.get('deleted', None), item['type'], item.get('by', None), item['time'], item.get('text', None),
-          item.get('dead', None), item.get('parent', None), item.get(
-              'poll', None), item.get('url', None),
-          item.get('score', None), item.get('title', None), ','.join(
-              map(str, item.get('parts', []))) if item.get('parts') else None,
-          item.get('descendants', None))):
+    """, items_data):
         await db.commit()
 
-    if 'kids' in item:
-        for order, kid_id in enumerate(item['kids']):
-            async with db.execute("""
-                INSERT INTO kids (item, kid, display_order)
-                VALUES (?, ?, ?)
-            """, (item['id'], kid_id, order)):
-                await db.commit()
+    async with db.executemany("""
+        INSERT INTO kids (item, kid, display_order)
+        VALUES (?, ?, ?)
+    """, kids_data):
+        await db.commit()
 
 
 async def main():
-    async with aiohttp.ClientSession() as session:
+    connector = aiohttp.TCPConnector(limit=NUM_WORKERS)
+    async with aiohttp.ClientSession(connector=connector) as session:
         async with aiosqlite.connect("hn_data.db") as db:
-            sem = asyncio.Semaphore(PARALLEL)
-            progress_bar = tqdm(total=(START_ID - END_ID),
-                                desc="Fetching items", unit="item")
-
+            sem = asyncio.Semaphore(NUM_WORKERS)
+            progress_bar = tqdm(total=(START_ID - END_ID))
             # Some optimizations for fast inserts at cost of resilience
             await db.execute("PRAGMA synchronous = OFF;")
             await db.execute("PRAGMA journal_mode = WAL;")
             await db.execute("PRAGMA cache_size = 1000000;")
             await db.execute("PRAGMA temp_store = MEMORY;")
             await db.execute("PRAGMA locking_mode = EXCLUSIVE;")
+            await db.execute("PRAGMA mmap_size = 30000000000;")
 
             async def process_item(item_id):
                 async with sem:
                     item = await fetch_item(session, item_id)
                     if item:
-                        await insert_item(db, item)
-                        progress_bar.update(1)
+                        return item
+                    return None
 
+            items_batch = []
             for i in range(START_ID, END_ID - BATCH_SIZE, -BATCH_SIZE):
                 tasks = [process_item(item_id) for item_id in range(
                     i, max(i - BATCH_SIZE, END_ID - 1), -1)]
-                await asyncio.gather(*tasks)
+                items = await asyncio.gather(*tasks)
+                items_batch.extend(
+                    [item for item in items if item is not None])
+                await insert_items_batch(db, items)
+                progress_bar.update(len(items))
+
             progress_bar.close()
 
 if __name__ == "__main__":
