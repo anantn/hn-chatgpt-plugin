27f28ea3eac10cabab7ab1c84ccbd8b64b9a9971
Rukmani Ravi <lakhi.r@gmail.com>
2023-04-25 19:08:24 -0700

    Text search on title & text

--- api-server/crud.py
+++ api-server/crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
-from typing import Union
+from sqlalchemy import or_
+from typing import Union, Optional
 import models
 import schemas
 
@@ -12,8 +13,13 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 
-def get_items(db: Session, skip: int = 0, limit: int = 100, type: Union[models.ItemType, None] = None):
-    return db.query(models.Item).filter(models.Item.type == type).offset(skip).limit(limit).all()
+def get_items(db: Session, skip: int = 0, limit: int = 100, type: Union[models.ItemType, None] = None, query: Optional[str] = None):
+    db_query = db.query(models.Item)
+    if type is not None:
+        db_query = db_query.filter(models.Item.type == type)
+    if query is not None:
+        db_query = db_query.filter(or_(models.Item.title.contains(query), models.Item.text.contains(query)))
+    return db_query.offset(skip).limit(limit).all()
 
 
 def get_item(db: Session, item_id: int):
