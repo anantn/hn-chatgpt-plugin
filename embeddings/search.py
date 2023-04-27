4294ead986d512ebc0a8ea67e8bc6f12ccd0e4f1
Anant Narayanan <anant@kix.in>
2023-04-26 20:22:43 -0700

    Partition faiss index for faster search

--- embeddings/search.py
+++ embeddings/search.py
@@ -1,4 +1,5 @@
 import os
+import gc
 import sys
 import time
 import sqlite3
@@ -7,11 +8,17 @@ import faiss
 import psutil
 from InstructorEmbedding import INSTRUCTOR
 
+TOP_K = 50
+TOP_N = 10
+NLIST = 100
+start_time = time.time()
+
 
 def print_memory_usage(phase):
     process = psutil.Process(os.getpid())
     mem_info = process.memory_info()
-    print(f"Memory used ({phase}): {mem_info.rss / (1024 * 1024):.2f} MB")
+    elapsed = time.time() - start_time
+    print(f"{elapsed:.2f}s Memory used ({phase}): {mem_info.rss / (1024 * 1024):.2f} MB")
 
 
 def fetch_story_title(conn, story_id):
@@ -51,6 +58,18 @@ def load_embeddings(embeddings_db_path):
     return embeddings, item_ids, dim
 
 
+def create_ivf_flat_index(embeddings):
+    dim = embeddings.shape[1]
+    quantizer = faiss.IndexFlatL2(dim)
+    index = faiss.IndexIVFFlat(quantizer, dim, NLIST, faiss.METRIC_L2)
+    return index
+
+
+def embed_query(query, model):
+    return model.encode(
+        [['Represent the question for retrieving supporting forum discussions: ', query]])[0]
+
+
 def main():
     if len(sys.argv) < 2:
         print("Usage: python search.py <db_path>")
@@ -63,17 +82,25 @@ def main():
     prefix = os.path.splitext(expanded_db_path)[0]
     embeddings_db_path = f"{prefix}_embeddings.db"
     embeddings, item_ids, dim = load_embeddings(embeddings_db_path)
-    print_memory_usage("after loading")
+    print_memory_usage(f"after loading {len(embeddings)} embeddings")
 
     # Create a FAISS index and add the embeddings
-    index = faiss.IndexFlatL2(dim)
+    index = create_ivf_flat_index(embeddings)
+    index.train(embeddings)
+    index.nprobe = 20
+    print_memory_usage("after training")
     index_with_ids = faiss.IndexIDMap(index)
     index_with_ids.add_with_ids(embeddings, item_ids)
+    embeddings = None
+    gc.collect()
     print_memory_usage("after indexing")
 
     # Load instructor model
     model = INSTRUCTOR('hkunlp/instructor-large')
     print_memory_usage("after loading instructor-large")
+    # Warmup query
+    embed_query("query", model)
+    print_memory_usage("after warmup query")
 
     # Query loop
     items_conn = sqlite3.connect(f"file:{expanded_db_path}?mode=ro", uri=True)
@@ -87,29 +114,29 @@ def main():
 
         # Encode the query
         start_time = time.time()
-        query_embedding = model.encode(
-            [['Represent the question for retrieving supporting forum discussions: ', query]])[0]
+        query_embedding = embed_query(query, model)
         end_time = time.time()
         print(f"Embedding generation took {end_time - start_time:.2f} seconds")
 
         # Search the FAISS index
-        k = 25
+
         start_time = time.time()
         distances, indices = index_with_ids.search(
-            np.array([query_embedding]), k)
+            np.array([query_embedding]), TOP_K)
         end_time = time.time()
         print(f"FAISS search took {end_time - start_time:.2f} seconds")
 
         # Print the titles of the top 5 unique stories
-        print("\nTop 5 unique stories for the query:")
+        print(f"\nTop {TOP_N} unique stories for the query:")
         unique_story_ids = set()
         count = 0
-        for story_id in indices[0]:
+        for story_id, l2_distance in zip(indices[0], distances[0]):
             if story_id not in unique_story_ids:
                 unique_story_ids.add(story_id)
-                print(fetch_story_title(items_conn, int(story_id)))
+                title = fetch_story_title(items_conn, int(story_id))
+                print(f"{title} {story_id} ({l2_distance:.2f})")
                 count += 1
-                if count == 5:
+                if count == TOP_N:
                     break
 
 
